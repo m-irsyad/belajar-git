8bf32d5a82d3504b3d8eab8afa12110eca133fc2
m-irsyad <78720029+m-irsyad@users.noreply.github.com>
2021-02-08 19:52:33 +0700

    Create index.php

---
+++ index/index.php
@@ -0,0 +1,103 @@
+<?php
+
+// function to encrypt the text given
+function encrypt($pswd, $text)
+{
+	// change key to lowercase for simplicity
+	$pswd = strtolower($pswd);
+	
+	// intialize variables
+	$code = "";
+	$ki = 0;
+	$kl = strlen($pswd);
+	$length = strlen($text);
+	
+	// iterate over each line in text
+	for ($i = 0; $i < $length; $i++)
+	{
+		if (ctype_alpha($text[$i]))
+		{
+			if (ctype_upper($text[$i]))
+			{
+				$text[$i] = chr(((ord($pswd[$ki]) - ord("a") + ord($text[$i]) - ord("A")) % 26) + ord("A"));
+			}
+			
+			else
+			{
+				$text[$i] = chr(((ord($pswd[$ki]) - ord("a") + ord($text[$i]) - ord("a")) % 26) + ord("a"));
+			}
+			
+			$ki++;
+			if ($ki >= $kl)
+			{
+				$ki = 0;
+			}
+		}
+	}
+	
+	// return the encrypted code
+	return $text;
+}
+
+// function to decrypt the text given
+function decrypt($pswd, $text)
+{
+	// change key to lowercase for simplicity
+	$pswd = strtolower($pswd);
+	
+	// intialize variables
+	$code = "";
+	$ki = 0;
+	$kl = strlen($pswd);
+	$length = strlen($text);
+	
+	// iterate over each line in text
+	for ($i = 0; $i < $length; $i++)
+	{
+		// if the letter is alpha, decrypt it
+		if (ctype_alpha($text[$i]))
+		{
+			// uppercase
+			if (ctype_upper($text[$i]))
+			{
+				$x = (ord($text[$i]) - ord("A")) - (ord($pswd[$ki]) - ord("a"));
+				
+				if ($x < 0)
+				{
+					$x += 26;
+				}
+				
+				$x = $x + ord("A");
+				
+				$text[$i] = chr($x);
+			}
+			
+			// lowercase
+			else
+			{
+				$x = (ord($text[$i]) - ord("a")) - (ord($pswd[$ki]) - ord("a"));
+				
+				if ($x < 0)
+				{
+					$x += 26;
+				}
+				
+				$x = $x + ord("a");
+				
+				$text[$i] = chr($x);
+			}
+			
+			// update the index of key
+			$ki++;
+			if ($ki >= $kl)
+			{
+				$ki = 0;
+			}
+		}
+	}
+	
+	// return the decrypted text
+	return $text;
+}
+
+?>

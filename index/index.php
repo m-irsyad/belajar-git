b1738c6503c419773c5093f91e307f65139e0385
m-irsyad <78720029+m-irsyad@users.noreply.github.com>
2021-02-09 16:19:01 +0700

    Update index.php

--- index/index.php
+++ index/index.php
@@ -29,7 +29,7 @@
 <body>
 	<div class="container">
 		<div class="row">
-			<div class="col" style="border:solid 1px">
+			<div class="col">
 				<h1>1 on 1 tutoring</h1>
 				<p class="text-muted">
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -37,18 +37,18 @@
 				<button type="button" class="btn btn-primary" id="round-btn">Free Consultation</button>
 				<button type="button" class="btn btn-dark" id="round-btn">Browse Tutor</button>
 			</div>
-			<div class="col" style="border:solid 1px">
+			<div class="col">
 			Illustration
 			</div>
 		</div>
 		<div class="row">
-			<div class="col" style="border:solid 1px">
+			<div class="col">
 			<h1>Our Process & Philosophy</h1>
 			<p class="text-muted">
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 			</p>
 			</div>
-			<div class="col" style="border:solid 1px">
+			<div class="col">
 			<h3>"I can't believe how fast my scores improved!"</h1>
 			<p class="text-muted">
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -71,19 +71,19 @@
 			</div>
 		</div>
 		<div class="row center">
-			<div class="col" style="border:solid 1px">
+			<div class="col">
 				<img src="../imgs/user.png" alt="" srcset="" class="rounded-circle team">
 				<p class="text-muted">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 				</p>
 			</div>
-			<div class="col" style="border:solid 1px">
+			<div class="col">
 				<img src="../imgs/user.png" alt="" srcset="" class="rounded-circle team">
 				<p class="text-muted">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 				</p>
 			</div>
-			<div class="col" style="border:solid 1px">
+			<div class="col">
 				<img src="../imgs/user.png" alt="" srcset="" class="rounded-circle team">
 				<p class="text-muted">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -91,7 +91,7 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col center" style="border:solid 1px">
+			<div class="col center">
 				<h1>Let's learn together</h1>
 				<p class="text-muted">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.

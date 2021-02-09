bd6c9f88c7a172c473847905f132423e47e43cb5
m-irsyad <78720029+m-irsyad@users.noreply.github.com>
2021-02-09 15:52:36 +0700

    Update index.php

--- index/index.php
+++ index/index.php
@@ -1,8 +1,85 @@
 <!doctype html>
 <head>
 	<title>grid</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	<style>
+		#round-btn {
+			border-radius:25px;
+		}
+		.blue-col {
+			background:blue;
+			border-radius:50px;
+			color:white;
+			text-align:center;
+			padding:30px;
+		}
+		.team {
+			background:grey;
+			width:100px;
+			height:100px;
+		}
+	</style>
 </head>
 <body>
-	<h1>test</h1>
+	<div class="container">
+		<div class="row">
+			<div class="col" style="border:solid 1px">
+				<h1>1 on 1 tutoring</h1>
+				<p class="text-muted">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<button type="button" class="btn btn-primary" id="round-btn">Free Consultation</button>
+				<button type="button" class="btn btn-dark" id="round-btn">Browse Tutor</button>
+			</div>
+			<div class="col" style="border:solid 1px">
+			Illustration
+			</div>
+		</div>
+		<div class="row">
+			<div class="col" style="border:solid 1px">
+			<h1>Our Process & Philosophy</h1>
+			<p class="text-muted">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			</div>
+			<div class="col" style="border:solid 1px">
+			<h3>"I can't believe how fast my scores improved!"</h1>
+			<p class="text-muted">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col blue-col">
+				<h1>Schedule today</h1>
+				<div class="row">
+					<div class="col">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</p>
+					</div>
+					<div class="col-3">
+					<button type="button" class="btn btn-dark" id="round-btn">Select Date Time</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col" style="border:solid 1px">
+			<img src="../coffee18.png" alt="" srcset="" class="rounded-circle team">
+			</div>
+			<div class="col" style="border:solid 1px">
+			One of three columns
+			</div>
+			<div class="col" style="border:solid 1px">
+			One of three columns
+			</div>
+		</div>
+		<div class="row">
+			<div class="col" style="border:solid 1px">
+			One columns
+			</div>
+		</div>
+	</div>
 </body>
 </html>

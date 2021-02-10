48d4441ddb7a79e5691022509b946a710b092459
m-irsyad <78720029+m-irsyad@users.noreply.github.com>
2021-02-10 11:54:52 +0700

    Update index.php

--- index/index.php
+++ index/index.php
@@ -2,15 +2,17 @@
 <head>
 	<title>grid</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	<script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+	<link href='https://fonts.googleapis.com/css?family=Markazi Text|Prata' rel='stylesheet'>
 	<style>
 		.row {
 			margin-top:50px;
 		}
-		#round-btn {
+		.round-btn {
 			border-radius:25px;
 		}
 		.blue-col {
-			background:blue;
+			background:#0d6efd;
 			border-radius:50px;
 			color:white;
 			text-align:center;
@@ -24,32 +26,71 @@
 		.center {
 			text-align:center;
 		}
+		.blue-icon {
+			color:#0d6efd;
+		}
+		.header1 {
+			font-family:'Markazi Text';
+			font-size:60px;
+		}
+		.header2 {
+			font-family:'Prata';
+		}
+		.left-border {
+			border-left: solid orange 5px;
+			padding-left:10px;
+		}
 	</style>
 </head>
 <body>
 	<div class="container">
 		<div class="row">
 			<div class="col">
-				<h1>1 on 1 tutoring</h1>
+				<h1 class="header1">1 on 1 Tutoring</h1>
 				<p class="text-muted">
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 				</p>
-				<button type="button" class="btn btn-primary" id="round-btn">Free Consultation</button>
-				<button type="button" class="btn btn-dark" id="round-btn">Browse Tutor</button>
+				<button type="button" class="btn btn-primary round-btn" style="margin-right:50px;">FREE CONSULTATION</button>
+				<button type="button" class="btn btn-dark round-btn">BROWSE TUTOR</button>
 			</div>
 			<div class="col">
-			Illustration
+			<img src="https://storyset.com/illustration/teacher/rafiki#0D6EFDFF&hide=&hide=complete" alt="" srcset="">
 			</div>
 		</div>
 		<div class="row">
 			<div class="col">
-			<h1>Our Process & Philosophy</h1>
-			<p class="text-muted">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			<h1 class="header2">Our Process & <br/>Philosophy</h1>
+			<p>
+				<span class="fa-stack fa-1x" style="vertical-align:middle;">
+					<i class="fa fa-circle fa-stack-2x blue-icon"></i>
+					<i class="fa fa-check fa-stack-1x fa-inverse"></i>
+				</span>
+				Lorem ipsum dolor sit amet
+			</p>
+			<p>
+				<span class="fa-stack fa-1x" style="vertical-align:middle;">
+					<i class="fa fa-circle fa-stack-2x blue-icon"></i>
+					<i class="fa fa-check fa-stack-1x fa-inverse"></i>
+				</span>
+				Lorem ipsum dolor sit amet
+			</p>
+			<p>
+				<span class="fa-stack fa-1x" style="vertical-align:middle;">
+					<i class="fa fa-circle fa-stack-2x blue-icon"></i>
+					<i class="fa fa-check fa-stack-1x fa-inverse"></i>
+				</span>
+				Lorem ipsum dolor sit amet
+			</p>
+			<p>
+				<span class="fa-stack fa-1x" style="vertical-align:middle;">
+					<i class="fa fa-circle fa-stack-2x blue-icon"></i>
+					<i class="fa fa-check fa-stack-1x fa-inverse"></i>
+				</span>
+				Lorem ipsum dolor sit amet
 			</p>
 			</div>
 			<div class="col">
-			<h3>"I can't believe how fast my scores improved!"</h1>
+			<h3 class="left-border">"I can't believe how fast my scores improved!"</h1>
 			<p class="text-muted">
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 			</p>
@@ -65,7 +106,7 @@
 						</p>
 					</div>
 					<div class="col-3">
-					<button type="button" class="btn btn-dark" id="round-btn">Select Date Time</button>
+					<button type="button" class="btn btn-dark round-btn">Select Date Time</button>
 					</div>
 				</div>
 			</div>
@@ -96,7 +137,7 @@
 				<p class="text-muted">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 				</p>
-				<button type="button" class="btn btn-primary" id="round-btn">Get Started</button>
+				<button type="button" class="btn btn-primary round-btn">Get Started</button>
 			</div>
 		</div>
 	</div>

63179a6520ee8f0732e2544900f4d98fd8378b9d
m-irsyad <78720029+m-irsyad@users.noreply.github.com>
2021-02-10 12:42:45 +0700

    Update index.php

--- index/index.php
+++ index/index.php
@@ -8,9 +8,6 @@
 		.row {
 			margin-top:50px;
 		}
-		.round-btn {
-			border-radius:25px;
-		}
 		.blue-col {
 			background:#0d6efd;
 			border-radius:50px;
@@ -18,13 +15,10 @@
 			text-align:center;
 			padding:30px;
 		}
-		.team {
-			background:grey;
-			height:100px;
-			width:100px;
-		}
 		.center {
 			text-align:center;
+			margin-left:auto;
+			margin-right:auto;
 		}
 		.blue-icon {
 			color:#0d6efd;
@@ -40,6 +34,14 @@
 			border-left: solid orange 5px;
 			padding-left:10px;
 		}
+		.footer {
+			left: 0;
+			bottom: 0;
+			width: 100%;
+			background-color: red;
+			color: white;
+			text-align: center;
+		}
 	</style>
 </head>
 <body>
@@ -50,15 +52,15 @@
 				<p class="text-muted">
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 				</p>
-				<button type="button" class="btn btn-primary round-btn" style="margin-right:50px;">FREE CONSULTATION</button>
-				<button type="button" class="btn btn-dark round-btn">BROWSE TUTOR</button>
+				<button type="button" class="btn btn-primary rounded-pill" style="margin-right:50px;">FREE CONSULTATION</button>
+				<button type="button" class="btn btn-dark rounded-pill">BROWSE TUTOR</button>
 			</div>
 			<div class="col">
-			<img src="https://storyset.com/illustration/teacher/rafiki#0D6EFDFF&hide=&hide=complete" alt="" srcset="">
+				<img src="../imgs/Teacher-rafiki.png" width=500px>
 			</div>
 		</div>
 		<div class="row">
-			<div class="col">
+			<div class="col-6">
 			<h1 class="header2">Our Process & <br/>Philosophy</h1>
 			<p>
 				<span class="fa-stack fa-1x" style="vertical-align:middle;">
@@ -89,11 +91,11 @@
 				Lorem ipsum dolor sit amet
 			</p>
 			</div>
-			<div class="col">
-			<h3 class="left-border">"I can't believe how fast my scores improved!"</h1>
-			<p class="text-muted">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</p>
+			<div class="col-4">
+				<h3 class="left-border">"I can't believe how fast my scores improved!"</h1>
+				<p class="text-muted">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
 			</div>
 		</div>
 		<div class="row">
@@ -106,40 +108,60 @@
 						</p>
 					</div>
 					<div class="col-3">
-					<button type="button" class="btn btn-dark round-btn">Select Date Time</button>
+					<button type="button" class="btn btn-dark rounded-pill">Select Date Time</button>
 					</div>
 				</div>
 			</div>
 		</div>
 		<div class="row center">
 			<div class="col">
-				<img src="../imgs/user.png" alt="" srcset="" class="rounded-circle team">
+				<span class="fa-stack fa-3x" style="vertical-align:middle;">
+					<i class="fa fa-circle fa-stack-2x"></i>
+					<i class="fa fa-user fa-stack-1x fa-inverse"></i>
+				</span>
 				<p class="text-muted">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 				</p>
 			</div>
 			<div class="col">
-				<img src="../imgs/user.png" alt="" srcset="" class="rounded-circle team">
+				<span class="fa-stack fa-3x" style="vertical-align:middle;">
+					<i class="fa fa-circle fa-stack-2x"></i>
+					<i class="fa fa-user fa-stack-1x fa-inverse"></i>
+				</span>
 				<p class="text-muted">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 				</p>
 			</div>
 			<div class="col">
-				<img src="../imgs/user.png" alt="" srcset="" class="rounded-circle team">
+				<span class="fa-stack fa-3x" style="vertical-align:middle;">
+					<i class="fa fa-circle fa-stack-2x"></i>
+					<i class="fa fa-user fa-stack-1x fa-inverse"></i>
+				</span>
 				<p class="text-muted">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 				</p>
 			</div>
 		</div>
 		<div class="row">
-			<div class="col center">
+			<div class="col-6 center">
 				<h1>Let's learn together</h1>
 				<p class="text-muted">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 				</p>
-				<button type="button" class="btn btn-primary round-btn">Get Started</button>
+				<button type="button" class="btn btn-primary rounded-pill">Get Started</button>
+				<div class="row">
+					<div class="col">
+						<i class="fa fa-mobile-alt fa-3x"></i>
+					</div>
+					<div class="col">
+						<i class="fa fa-comment fa-3x"></i>
+					</div>
+				</div>
 			</div>
 		</div>
+	</div>
+	<div class="footer">
+		asd
 	</div>
 </body>
 </html>

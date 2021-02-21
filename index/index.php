2cb66f37408a7f2bf071cba0135d96c99c67dae1
m-irsyad <78720029+m-irsyad@users.noreply.github.com>
2021-02-21 23:34:10 +0700

    Update index.php

--- index/index.php
+++ index/index.php
@@ -3,7 +3,7 @@
 	<title>grid</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 	<script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
-	<link href='https://fonts.googleapis.com/css?family=Markazi Text|Prata' rel='stylesheet'>
+	<link href='https://fonts.googleapis.com/css?family=Markazi Text|Prata|Bodoni Moda' rel='stylesheet'>
 	<style>
 		.row {
 			margin-top:50px;
@@ -12,8 +12,8 @@
 			background:#0d6efd;
 			border-radius:50px;
 			color:white;
-			text-align:center;
-			padding:30px;
+			text-align:left;
+			padding:100px;
 		}
 		.center {
 			text-align:center;
@@ -39,6 +39,9 @@
 			bottom: 0;
 			width: 100%;
 			clear:both;
+		}
+		.rounded-pill {
+			font-size:15px;
 		}
 	</style>
 </head>
@@ -90,8 +93,8 @@
 			</p>
 			</div>
 			<div class="col-5">
-				<h3 class="left-border">"I can't believe how fast my scores improved!"</h1>
-				<p class="text-muted"">
+				<h2 class="left-border" style="font-family:'Prata'">"I can't believe how fast my scores improved!"</h1>
+				<p class="text-muted">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 				</p>
 				<button type="button" class="btn btn-primary rounded-pill">VIEW TESTIMONIALS</button>
@@ -99,7 +102,7 @@
 		</div>
 		<div class="row">
 			<div class="col blue-col">
-				<h1>Schedule today</h1>
+				<h2 class="header2">Schedule a free consultation today</h1>
 				<div class="row">
 					<div class="col">
 						<p>

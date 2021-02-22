b6c68a55aaa8167ae20bae668594a093c3f2e733
m-irsyad <78720029+m-irsyad@users.noreply.github.com>
2021-02-22 23:34:39 +0700

    migrate-refresh

--- app/Database/Migrations/2021-02-22-055913_News.php
+++ app/Database/Migrations/2021-02-22-055913_News.php
@@ -32,6 +32,7 @@ class News extends Migration
 				'constraint'     => ['published', 'draft'],
 				'default'        => 'draft',
 			],
+			'created_at DATETIME DEFAULT CURRENT_TIMESTAMP'
 		]);
 
 		// Membuat primary key
